ae282a3290805d3796f6134d14c0b1002b60962d
kovacsgergely22 <113249160+kovacsgergely22@users.noreply.github.com>
2024-05-11 17:20:28 +0200

    add

---
+++ mktjs0_0503/feldolgozo.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <font style="font-size: 18px;">
+    <h2>HTML5 űrlap_mktjs0</h2>
+
+    <?php
+
+    echo ("<strong> Név: </strong>" . $_POST["nev"] . "<br><br>");
+    echo ("<strong> PIN kód: </strong>" . $_POST["nev"] . "<br><br>");
+    echo ("<strong> Gyümölcs: </strong>" . $_POST["nev"] . "<br><br>");
+    echo ("<strong> Életkor: </strong>" . $_POST["nev"] . "<br><br>");
+    echo ("<strong> Lábméret: </strong>" . $_POST["nev"] . "<br><br>");
+    echo ("<strong> Önbizalom: </strong>" . $_POST["nev"] . "<br><br>");
+
+    ?>
+    <a href="urlap_mktjs0.html">Vissza az űrlapra...</a>
+    </font>
+    
+</body>
+</html>
